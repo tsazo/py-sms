ffe85377b523c298dbbae8060c2dea1037adfe37
Trinity Sazo <tsazo@andover.edu>
2016-07-25 09:17:43 -0400

    Sending texts

--- send_sms.py
+++ send_sms.py
@@ -6,11 +6,11 @@ from random import choice
 ######################################################
 # Warning: you must replace the values below!
 # Find these values at https://twilio.com/user/account
-account_sid = "AC3b871b8dc7bb3785cddda6fd9ad271fe"
-auth_token = "6b65df96c48e7d3b99ac574d872af60a"  
-fromnumber = "+17815705388"
-tonumber = "+17816402658"
-body_text = "Hi!  I'm a sample text message!"
+account_sid = "AC6842272e7608425c7c5757bdb6c2a2eb"
+auth_token = "80ca0459e2015dde581e70ef7fc419e0"  
+fromnumber = "+19787889940"
+tonumber = "" #your number here
+body_text = "Hi."
 ######################################################
 
 client = TwilioRestClient(account_sid, auth_token)
@@ -35,13 +35,14 @@ def send_media(fromnumber, tonumber, media_links):
 
 	print "Media text successfully sent!"
 
-send(fromnumber, tonumber, body_text)
+#send(fromnumber, tonumber, body_text)
 
-# media_links = ["http://i.telegraph.co.uk/multimedia/archive/03597/POTD_chick_3597497k.jpg", 'https://upload.wikimedia.org/wikipedia/commons/5/55/Phillips_Academy_Andover_Coat_of_Arms.png', 'http://vignette1.wikia.nocookie.net/pokemon/images/f/fc/025Pikachu_OS_anime_5.png/revision/20150101093704', "http://all4desktop.com/data_images/original/4238212-pictures.jpg", "http://i.telegraph.co.uk/multimedia/archive/03519/potd-squirrel_3519920k.jpg", "http://i.telegraph.co.uk/multimedia/archive/03571/potd-squirrel_3571152k.jpg"]
-# send_media(fromnumber, tonumber, choice(media_links))
+media_links = ["http://i.telegraph.co.uk/multimedia/archive/03597/POTD_chick_3597497k.jpg", 'https://upload.wikimedia.org/wikipedia/commons/5/55/Phillips_Academy_Andover_Coat_of_Arms.png', 'http://vignette1.wikia.nocookie.net/pokemon/images/f/fc/025Pikachu_OS_anime_5.png/revision/20150101093704', "http://all4desktop.com/data_images/original/4238212-pictures.jpg", "http://i.telegraph.co.uk/multimedia/archive/03519/potd-squirrel_3519920k.jpg", "http://i.telegraph.co.uk/multimedia/archive/03571/potd-squirrel_3571152k.jpg"]
+send_media(fromnumber, tonumber, choice(media_links))
 
 # user = raw_input("Enter a text to send your TWILIO number.  Or enter q to quit: \n> ")
 # while user != "q":
 # 	response = response_handler(user)
 # 	send(fromnumber, tonumber, response)
 # 	user = raw_input("Enter a text to send your TWILIO number.  Or enter q to quit: \n> ")
+
