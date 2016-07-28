d11197a4158f5fda0a0e5075e93a5ea4c6abcef6
Trinity Sazo <tsazo@andover.edu>
2016-07-28 09:13:59 -0400

    group text

--- handler.py
+++ handler.py
@@ -1,15 +1,36 @@
 def response_handler(body):
-    message = ""
-    if body == 'start':
-        message = "You are in a boring class.  Do you 'take a nap' or 'take notes'?"
-    elif body == 'take a nap':
-        message = "You fall asleep forever...and ever...and ever.  Do you want to 'start' over?"
-    elif body == 'take notes':
-        message = "You furiously take notes as the teacher drones on.  Your friend taps you on the shoulder.  Do you 'turn around' or keep looking at your 'notes'"
-    elif body == 'turn around':
-        message = "You turn around, only to find that you have just stared into the eyes of a basilisk!  Oh well...do you want to 'start' over?"
-    elif body == 'notes':
-        message = "Unfortunately, this game is incomplete...please come back later for more!  Do you want to 'start' over?"
+    message = "Type 'music' if you want some music to hear or 'food ideas' if you don't know what to eat today:"
+    if body == 'music:sad':
+        message = "How are you feeling today?"    
+        if music == 'music:sad':
+            message = "Here are some music suggestions: "
+            quotes = ["", "", ""]
+            send_media(choice(quotes))
+        elif music == 'happy':
+            message = "Here are some music suggestions: "
+            quotes = ["", "", ""]
+            send_media(choice(quotes))
+        elif music == 'angry':
+            message = "Here are some music suggestions: "
+            quotes = ["", "", ""]
+            send_media(choice(quotes))
+        elif music == 'inspired':
+            message = "Here are some music suggestions: "
+            quotes = ["", "", ""]
+            send_media(choice(quotes))
+        elif music == 'Grieving Heart':
+            message == "Here are some music suggestions: "
+            quotes = ["", "", ""]
+            send_media(choice(quotes))
+        elif music == "Falling in Love":
+            message = "Here are some music suggestions: "
+        elif music == 'throwback':
+            message = "Here are some music suggestions: "
+        elif music == 'gym motivation':
+            message = "Here are some music suggestions: "
+    elif body == 'food ideas':
+        message = "hi"
     else:
-        message = "Invalid command.  Text 'start' to restart the game.  Or text 'pic please' for a random picture"
+        message = "hi"
     return message
+
--- send_sms.py
+++ send_sms.py
@@ -9,7 +9,7 @@ from random import choice
 account_sid = "AC6842272e7608425c7c5757bdb6c2a2eb"
 auth_token = "80ca0459e2015dde581e70ef7fc419e0"  
 fromnumber = "+19787889940"
-tonumber = "" #your number here
+tonumber = "+19783371696" #your number here
 body_text = "Hi."
 ######################################################
 
@@ -26,23 +26,23 @@ def send(fromnumber, tonumber, body_text):
 	print "SMS text successfully sent!"
 
 # sends a media text from fromnumber to tonumber consisting of media_links (list of links to media)
-def send_media(fromnumber, tonumber, media_links):
-	print "Preparing to send media text from %s to %s: %s" % (fromnumber, tonumber, media_links)
+# def send_media(fromnumber, tonumber, media_links):
+# 	print "Preparing to send media text from %s to %s: %s" % (fromnumber, tonumber, media_links)
 
-	message = client.messages.create(to=tonumber, 
-									 from_=fromnumber, 
-									 media_url=media_links)
+# 	message = client.messages.create(to=tonumber, 
+# 									 from_=fromnumber, 
+# 									 media_url=media_links)
 
-	print "Media text successfully sent!"
+# 	print "Media text successfully sent!"
 
 #send(fromnumber, tonumber, body_text)
 
-media_links = ["http://i.telegraph.co.uk/multimedia/archive/03597/POTD_chick_3597497k.jpg", 'https://upload.wikimedia.org/wikipedia/commons/5/55/Phillips_Academy_Andover_Coat_of_Arms.png', 'http://vignette1.wikia.nocookie.net/pokemon/images/f/fc/025Pikachu_OS_anime_5.png/revision/20150101093704', "http://all4desktop.com/data_images/original/4238212-pictures.jpg", "http://i.telegraph.co.uk/multimedia/archive/03519/potd-squirrel_3519920k.jpg", "http://i.telegraph.co.uk/multimedia/archive/03571/potd-squirrel_3571152k.jpg"]
-send_media(fromnumber, tonumber, choice(media_links))
+# media_links = ["http://i.telegraph.co.uk/multimedia/archive/03597/POTD_chick_3597497k.jpg", 'https://upload.wikimedia.org/wikipedia/commons/5/55/Phillips_Academy_Andover_Coat_of_Arms.png', 'http://vignette1.wikia.nocookie.net/pokemon/images/f/fc/025Pikachu_OS_anime_5.png/revision/20150101093704', "http://all4desktop.com/data_images/original/4238212-pictures.jpg", "http://i.telegraph.co.uk/multimedia/archive/03519/potd-squirrel_3519920k.jpg", "http://i.telegraph.co.uk/multimedia/archive/03571/potd-squirrel_3571152k.jpg"]
+# send_media(fromnumber, tonumber, choice(media_links))
 
-# user = raw_input("Enter a text to send your TWILIO number.  Or enter q to quit: \n> ")
-# while user != "q":
-# 	response = response_handler(user)
-# 	send(fromnumber, tonumber, response)
-# 	user = raw_input("Enter a text to send your TWILIO number.  Or enter q to quit: \n> ")
+user = raw_input("Type music: \n> ")
+while user != "q":
+	response = response_handler(user)
+	send(fromnumber, tonumber, response)
+	user = raw_input("Hello: \n> ")
 
